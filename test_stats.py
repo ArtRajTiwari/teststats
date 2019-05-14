c3ec499fbfadc45818b9bdc19eb20bf236129053
art <artrajtiwari@gmail.com>
2019-05-14 21:11:54 +0545

    test1

---
+++ test_stats.py
@@ -0,0 +1,20 @@
+import math
+
+
+def steam_leaf(array_of_dat):
+    d = []
+    for num in array_of_dat:
+        def first_n_digits(num, n=1):
+            return num // 10 ** (int(math.log(num, 10)) - n + 1)
+
+        first_digit = first_n_digits(num)
+        print(str(d))
+
+        if num in range((first_digit * 10) - 9, (first_digit * 10)):
+            d[str(first_digit)] = str(num)
+
+    return d
+
+
+print(steam_leaf([15, 23, 34, 23, 11, 10, 12, 14, 30, 32]))
+# steam_leaf([21,22,12,13,14])
