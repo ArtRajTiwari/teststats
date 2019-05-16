4b34fd0d97e86505ee0c7cb972406c360769cd15
art <artrajtiwari@gmail.com>
2019-05-16 14:04:40 +0545

    test1

--- test_test.py
+++ test_test.py
@@ -1,13 +1,15 @@
 from collections import defaultdict
+class dictdict():
+    pass
 
 
 def steam_leaf(data):
     stem_and_leaf = defaultdict(list)
     for numbers in data:
-        first_dig = int(str(numbers)[:1])
-        steam = int(str(numbers)[1::])
-        if steam not in stem_and_leaf[first_dig]:
-            stem_and_leaf[first_dig].append(steam)
+        stem = int(str(numbers)[:1])
+        leaf = int(str(numbers)[1::])
+        if leaf not in stem_and_leaf[stem]:
+            stem_and_leaf[stem].append(leaf)
 
     return stem_and_leaf
 
